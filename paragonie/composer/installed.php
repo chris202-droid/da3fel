2d054af7ae3408504093e42925d1195cc8e06678
christophe <67082914+chris202-droid@users.noreply.github.com>
2021-08-26 14:10:04 +0100

    Add files via upload

---
+++ paragonie/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'paragonie/constant_time_encoding' => 
+    array (
+      'pretty_version' => 'v1.0.4',
+      'version' => '1.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2132f0f293d856026d7d11bd81b9f4a23a1dc1f6',
+    ),
+    'paragonie/random_compat' => 
+    array (
+      'pretty_version' => 'v2.0.19',
+      'version' => '2.0.19.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '446fc9faa5c2a9ddf65eb7121c0af7e857295241',
+    ),
+    'phpseclib/phpseclib' => 
+    array (
+      'pretty_version' => '3.0.5',
+      'version' => '3.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7c751ea006577e4c2e83326d90c8b1e8c11b8ede',
+    ),
+    'twilio/sdk' => 
+    array (
+      'pretty_version' => '5.42.2',
+      'version' => '5.42.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0cfcb871b18a9c427dd9e8f0ed7458d43009b48a',
+    ),
+  ),
+);
